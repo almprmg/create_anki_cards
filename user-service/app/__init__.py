c76655e15fa612935c471ea47e61d8f42f250553
almprmg <hhyyuu0990@gmail.com>
2025-04-07 21:01:44 +0300

    fix: load environment variables and update database URI configuration for PostgreSQL

--- user-service/app/__init__.py
+++ user-service/app/__init__.py
@@ -7,16 +7,17 @@ from flask_migrate import Migrate
 from .config import Config
 from .database import init_db ,db_session
 from .auth import auth_blueprint
-
+from dotenv import load_dotenv
 # from .routes import user_blueprint
-
+load_dotenv()
 server = Flask(__name__)
 server.config.from_object(Config)
 server.config['JWT_SECRET_KEY'] = os.environ.get("JWT_SECRET")
 server.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(days=1)
 server.config['JWT_REFRESH_TOKEN_EXPIRES'] = timedelta(days=7)
+server.config['JWT_VERIFY_SUB'] = False
 jwt = JWTManager(server)
-server.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://postgres:postgres@localhost:5432/postgres"#osenviron.get("DB_URL")
+server.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
 
 
