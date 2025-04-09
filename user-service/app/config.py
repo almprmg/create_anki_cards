0d242cc8e7d00e7a34aab7bdcfa44aa4b77ac4cb
almprmg <hhyyuu0990@gmail.com>
2025-04-09 15:51:04 +0300

    remove user-service and related files, including Docker configuration, database models, and authentication logic

--- user-service/app/config.py
+++
@@ -1,7 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://user:password@localhost:5432/user_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'your_secret_key'
-    MAX_ATTEMPTS = 5
-    LOCKOUT_TIME = 600 
-    TOKEN_EXPIRY = 3600  # ساعة واحدة
